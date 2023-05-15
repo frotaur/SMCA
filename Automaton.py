d92b7c819614cd2acbf3a7efc7051341fce68115
Vassilis <vassilis.papadopoulos@epfl.ch>
2023-05-15 20:31:15 +0200

    First commit

---
+++ Automaton.py
@@ -0,0 +1,97 @@
+import numpy as np
+from numba import njit, prange
+import numba.typed as numt
+
+class Automaton :
+    """
+        Class that internalizes the rules and evolution of 
+        the cellular automaton at hand. It has a step function
+        that makes one timestep of the evolution. By convention,
+        and to keep in sync with pygame, the world tensor has shape
+        (W,H,3). It contains float values between 0 and 1, which
+        are mapped to 0 255 when returning output, and describes how the
+        world is 'seen' by an observer.
+
+        Parameters :
+        size : 2-uple
+            Shape of the CA world
+        
+    """
+
+    def __init__(self,size):
+        self.w, self.h  = size
+        self.size= size
+        self._worldmap = np.random.uniform(size=(self.w,self.h,3))
+    
+
+    def step(self):
+        return NotImplementedError('Please subclass "Automaton" class, and define self.step')
+    
+    @property
+    def worldmap(self):
+        return (255*self._worldmap).astype(dtype=np.uint8)
+
+
+
+class SMCA(Automaton):
+    """
+        Standard Model Cellular Automaton. Inspired by LGCA.
+
+        Parameters :
+        <put them as I go>
+    """
+
+    def __init__(self, size):
+        super().__init__(size)
+        # 0,1,2,3 are  N,O,S,E directions
+        self.particles = np.random.randn(4,self.w,self.h) # (5,W,H)
+        self.particles[:,:,:]=0
+        self.particles = np.where(self.particles>1.4,1,0).astype(np.int16)
+        self.particles[1,100:190,40:60]=1
+
+        self.dir = numt.List([np.array([0,-1]),np.array([-1,0]),np.array([0,1]),np.array([1,0])])
+
+    
+    def collision_step(self):
+        self.particles=collision_numba(self.particles,self.w,self.h)
+        
+
+    def propagation_step(self):
+
+        self.particles=propagation_numba(self.particles,self.w,self.h,self.dir)
+        
+                    
+    def step(self):
+        self.collision_step()
+        self.propagation_step()
+
+        self._worldmap[:]=((self.particles.sum(axis=0)/4.)*255).astype(np.uint8)[:,:,None]
+
+
+@njit(parallel=True)
+def collision_numba(particles,w,h):
+    partictot = particles.sum(axis=0) # (W,H)
+    for x in prange(w):
+        for y in prange(h):
+            if(partictot[x,y]==2):
+                if(particles[0][x,y]==1 and particles[2][x,y]==1):
+                    particles[:,x,y]=np.array([0,1,0,1])
+                elif(particles[1][x,y]==1 and particles[3][x,y]==1):
+                    particles[:,x,y]=np.array([1,0,1,0])
+    
+    return particles
+
+@njit(parallel=True)
+def propagation_numba(particles,w,h,dirdico):
+    newparticles=np.copy(particles)
+
+    for x in prange(w):
+        for y in prange(h):
+            loc = np.array([x,y])
+            for dir in range(4):
+                if(particles[dir][x,y]==1):
+                    newpos = (loc+dirdico[dir])%np.array([w,h])
+                    newparticles[dir][newpos[0],newpos[1]]=1
+                    newparticles[dir][x,y]=0
+    
+    return newparticles
---
+++ main.py
@@ -0,0 +1,44 @@
+import torch
+import pygame
+from Camera import Camera
+from Automaton import *
+
+pygame.init()
+W,H = 200,200
+screen = pygame.display.set_mode((W,H))
+clock = pygame.time.Clock()
+running = True
+camera = Camera(W,H)
+
+world_state = torch.randint(0,255,(W,H,3),dtype=torch.uint8)
+
+auto = SMCA((W,H))
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        camera.handle_event(event)
+    
+
+    auto.step()
+    world_state = auto.worldmap
+    surface = pygame.surfarray.make_surface(world_state)
+
+    # Clear the screen
+    screen.fill((0, 0, 0))
+
+    # Draw the scaled surface on the window
+    zoomed_surface = camera.apply(surface)
+
+    screen.blit(zoomed_surface, (0,0))
+
+    # Update the screen
+    pygame.display.flip()
+    # flip() the display to put your work on screen
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
